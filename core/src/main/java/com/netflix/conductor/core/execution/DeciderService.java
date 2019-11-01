2648141feff17c77b3f27a70efcb6c705af42a0a
Kishore <kbanala@netflix.com>
2019-11-01 11:27:35 -0700

    Added optional async decide to decide the workflows through WorkflowSweeper.

--- core/src/main/java/com/netflix/conductor/core/execution/DeciderService.java
+++ core/src/main/java/com/netflix/conductor/core/execution/DeciderService.java
@@ -636,4 +636,23 @@ public class DeciderService {
         }
 
     }
+
+    /**
+     * Decide types with increasing order of priority.
+     */
+    public enum DecideType {
+        SWEEP(0),
+        UPDATE_TASK(1),
+        PARENT_WORKFLOW_DECIDE(1);
+
+        private final int priority;
+
+        DecideType(final int priority) {
+            this.priority = priority;
+        }
+
+        public int getPriority() {
+            return priority;
+        }
+    }
 }
